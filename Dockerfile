33c85a6e69c35fd88b23cb2fed898da6b84b145c
Michael Laccetti <michael@laccetti.com>
2017-03-19 11:54:50 -0400

    Wow, typos hurt

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,6 @@ FROM kelseyhightower/kube-cert-manager:0.5.0
 
 LABEL maintainer="Michael Laccetti (michael@laccetti.com)"
 
-ADD dist/route33 /route53
+ADD dist/route53 /route53
 
 ENTRYPOINT ["/kube-cert-manager"]
--- travis-build.sh
+++ travis-build.sh
@@ -8,10 +8,10 @@ fi
 
 DOCKER_TAG=$(sed 's/\//_/g' <<< ${TRAVIS_BRANCH})
 
-docker build -f Dockerfile.build -t mlaccetti/kube-cert-manager-r53:build-${DOCKER_TAG} .
+docker build --pull -f Dockerfile.build -t mlaccetti/kube-cert-manager-r53:build-${DOCKER_TAG} .
 docker run -v $(pwd):/go/src/github.com/mlaccetti/dns01-exec-plugin-r53 mlaccetti/kube-cert-manager-r53:build-${DOCKER_TAG}
 
-docker build -t mlaccetti/kube-cert-manager-r53:${DOCKER_TAG} .
+docker build --pull -t mlaccetti/kube-cert-manager-r53:${DOCKER_TAG} .
 
 docker login -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}"
 docker push mlaccetti/kube-cert-manager-r53:${DOCKER_TAG}
