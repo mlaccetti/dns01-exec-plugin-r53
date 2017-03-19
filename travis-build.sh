241afdc95a109ea47bd4a50a25119a2aa5fd0609
Michael Laccetti <michael@laccetti.com>
2017-03-19 11:33:41 -0400

    Let's get some CI running

---
+++ travis-build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# set -xe
+
+if [[ ${TRAVIS_TAG} ]]; then
+  TRAVIS_BRANCH=${TRAVIS_TAG}
+fi
+
+DOCKER_TAG=$(sed 's/\//_/g' <<< ${TRAVIS_BRANCH})
+
+docker build -f Dockerfile.build -t mlaccetti/kube-cert-manager-r53:build-${DOCKER_TAG} .
+docker run -v $(pwd):/go/src/github.com/mlaccetti/kube-cert-manager-r53 mlaccetti/kube-cert-manager-r53:build-${DOCKER_TAG}
+
+docker build -t mlaccetti/kube-cert-manager-r53:${DOCKER_TAG} .
+
+docker login -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}"
+docker push mlaccetti/kube-cert-manager-r53:${DOCKER_TAG}
